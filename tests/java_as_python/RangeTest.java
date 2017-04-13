84c75b64552def5c6d84db436f5f7f7b1ec4a36a
JPTIZ <jpaulotiz@gmail.com>
2017-04-13 11:49:33 -0300

    switching tabs with spaces

--- tests/java_as_python/RangeTest.java
+++ tests/java_as_python/RangeTest.java
@@ -8,22 +8,22 @@ import collections.Range;
 
 public class RangeTest {
 
-	@Test
-	public void simpleForEach() {
-		int i_ = 0;
-		for (int i: new Range(10)) {
-			assertEquals(i, i_);
-			i_++;
-		}
-	}
-	
-	@Test
-	public void minAndMaxRange() {
-		int i_ = 10;
-		for (int i: new Range(10, 30)) {
-			assertEquals(i, i_);
-			i_++;
-		}
-	}
+    @Test
+    public void simpleForEach() {
+        int i_ = 0;
+        for (int i: new Range(10)) {
+            assertEquals(i, i_);
+            i_++;
+        }
+    }
+    
+    @Test
+    public void minAndMaxRange() {
+        int i_ = 10;
+        for (int i: new Range(10, 30)) {
+            assertEquals(i, i_);
+            i_++;
+        }
+    }
 
 }
